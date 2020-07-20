dbe336f80e1c12d04a0d877712fa9d97e447ec2c
Dawid <d.lokiec93@gmail.com>
2020-07-20 13:30:44 +0200

    Implemented tests

---
+++ src/test/java/com/example/BaseTest.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.testkit.javadsl.TestKit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    private static ActorSystem actorSystem;
+
+    @BeforeClass
+    public static void setUp() {
+        actorSystem = ActorSystem.create("web_socket_test_automation_system");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        actorSystem.terminate();
+        actorSystem = null;
+    }
+
+    @Test
+    public void echoTest() {
+        new TestKit(actorSystem) {
+            {
+                final ActorRef clientReference = actorSystem.actorOf( Props.create(WebSocketClientActor.class) );
+                final String msg = "Hello Server";
+                // Stimulation: Send echo to server's web socket
+                clientReference.tell(msg, getRef() );
+                // Tests {
+                // The expected message should be equal to msg and arrived
+                expectMsg(Duration.ofSeconds(3), msg);
+                // No more messages are expected.
+                expectNoMessage();
+                // }
+            }
+        };
+    }
+
+}
